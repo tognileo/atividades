741c786a57430267f849140aeaca3ad3edda989c
tognileo <tognileo08@gmail.com>
2026-04-19 17:23:29 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,20 @@
+
+n1= int(input("digite um numero"))
+n2= int(input("digite outro numero"))
+
+soma = n1+n2
+subtracao = n1-n2
+divisao = n1/n2
+multiplicacao= n1*n2
+print(soma)
+print(subtracao)
+print(divisao)
+print(multiplicacao)
+
+
+#2222222222222222222222222222222222222222222222222222222
+
+n1= int(input("digite um numero"))
+for i in range (1,11):
+    conta=n1*i
+    print(conta)
